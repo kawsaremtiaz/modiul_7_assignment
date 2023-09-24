f339615ac4b2ef2e4a47d637c43799182be82ebe
Kawsar Emtiaz <kawsaremtiaz@Kawsars-MacBook-Pro.local>
2023-09-24 10:58:53 +0600

    Flutter Stateful Widgets & Counter App Project Create a Flutter application that consists of the following: A stateful widget called ProductList that displays a list of products. Each product should have a name, price, and a "Buy Now" button.
    Implement a counter for each product that increments by 1 every time the "Buy
    Now" button is pressed.
    When the counter for a product reaches 5, show a dialog box with the message
    "Congratulations! You've bought 5 {Product Name}!" Replace {Product Name} with
    the actual name of the product.
    
    Use the List View widget to display the product list.
    Implement navigation to a new page called Cartage when the user presses a "Go
    to Cart" button.
    
    The Cartage should display the total number of products the user has bought.

---
+++ android/app/src/main/kotlin/com/modiul7/modiul_7/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.modiul7.modiul_7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class Product {
+  final String name;
+  final double price;
+  int quantity;
+
+  Product(this.name, this.price, this.quantity);
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProductList(),
+    );
+  }
+}
+
+class ProductList extends StatefulWidget {
+  @override
+  _ProductListState createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  // //List<Product> products = List.generate(20, (index) => Product("Product", 10.0, 0));
+  //   List<Product> products = List.generate(20, (index) => Product("Product s", 10.0, 0));
+  List<Product> productss = [
+    Product("Product 1", 10.0, 0),
+    Product("Product 2", 15.0, 0),
+    Product("Product 3", 20.0, 0),
+    Product("Product 4", 10.0, 0),
+    Product("Product 5", 15.0, 0),
+    Product("Product 6", 20.0, 0),
+    Product("Product 7", 10.0, 0),
+    Product("Product 8", 15.0, 0),
+    Product("Product 9", 20.0, 0),
+    Product("Product 10", 10.0, 0),
+    Product("Product 11", 15.0, 0),
+    Product("Product 12", 20.0, 0),
+    Product("Product 13", 10.0, 0),
+    Product("Product 14", 15.0, 0),
+    Product("Product 15", 20.0, 0),
+    Product("Product 16", 10.0, 0),
+    Product("Product 17", 15.0, 0),
+    Product("Product 18", 20.0, 0),
+    Product("Product 19", 15.0, 0),
+    Product("Product 20", 20.0, 0),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product List'),
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => CartPage(productss),
+            ),
+          );
+        },
+        child: Icon(Icons.shopping_cart)
+      ),
+      body: ListView.builder(
+        itemCount: productss.length,
+        itemBuilder: (context, index) {
+          final product = productss[index];
+          return ListTile(
+            title: Text('${product.name}'),
+            subtitle: Text('\$${product.price.toStringAsFixed(2)}'),
+            trailing: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text('Counter ${product.quantity}'),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        product.quantity++;
+                        if (product.quantity == 5) {
+                          // Show the dialog when the counter reaches 5
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text('Congratulations!'),
+                                content: Text(
+                                    'You\'ve bought 5 ${product.name}!'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text('OK'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        }
+                      });
+                    },
+                    child: Text('Buy Now'),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CartPage extends StatelessWidget {
+  final List<Product> products;
+
+  CartPage(this.products);
+
+  int getTotalQuantity() {
+    int total = 0;
+    for (Product product in products) {
+      total += product.quantity;
+    }
+    return total;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cart'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Total Products in Cart: ${getTotalQuantity()}',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
